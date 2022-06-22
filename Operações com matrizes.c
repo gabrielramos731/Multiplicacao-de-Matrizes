80af78fd8bf989f755b7d701b719d5bd1e1a0f17
Gabriel Ramos <74663465+gabrielramos731@users.noreply.github.com>
2022-06-22 16:04:53 -0300

    Update and rename main.c to Operações com matrizes.c
    Faz a captura da quantidade de linha e colunas de uma matriz e captura seus valores.

--- Operações com matrizes.c
+++ Operações com matrizes.c
@@ -10,7 +10,27 @@ void Apresenta (int m[L][C]);
 void CriaOposta (int o[L][C], int m[L][C]);
 void Multiplica (int p[L][C], int m[L][C], int o[L][C]);
 
-int main() //Elis Lopes Silva Araújo - 12011FMT038 / Gabriel Ramos da Silva - 12011FMT013 / Gabriel Santos Zampier Colomer - 11811FMT012
+int main() //Gabriel Ramos da Silba - 06/2021
+{
+    scanf ("%d %d", &L, &C);
+
+    int m [L][C];   //matriz m
+    int o [L][C];   //matriz oposta
+    int p [L][C];   //matriz produto
+
+        if(L > MAX || C > MAX)
+    {
+        printf("Arquivo contém matriz maior que o suportado!\n");
+        return 1;
+    }
+
+    CarregaM (m);
+    Apresenta (m);
+    CriaOposta (o, m);
+    Multiplica (p, m, o);
+
+    return 0;
+}
 
 void
 CarregaM (int m[L][C]) //função para carregar a matriz contida no arquivo
@@ -21,23 +41,23 @@ CarregaM (int m[L][C]) //função para carregar a matriz contida no arquivo
     for (i = 0; i < L; i++)   //carrega a matriz m
         for (j = 0; j < C; j++)
             scanf ("%d", &m[i][j]);
-            
+
 }
 
 void
 Apresenta (int m[L][C]) //função que irá apresentar a matriz carregada
 {
     int i, j;   //controladores da matriz m
-    
+
     printf ("Matriz original \n");
     for (i = 0; i < L; i++)
     {
         for (j = 0; j < C; j++)
             printf ("%2d ", m[i][j]);
 
-        printf ("\n"); 
+        printf ("\n");
     }
-    
+
 }
 
 void
@@ -45,7 +65,7 @@ CriaOposta (int o[L][C], int m[L][C]) //função que irá gerar e apresentar a m
 {
     int i, j;   //controladores de m
     int q, r;   //controladores de o (oposta de m)
-    
+
     for (i = 0, q = 0; i < L; i++, q++)
     {
         for (j = 0, r = 0; j < C; j++, r++)
@@ -69,14 +89,14 @@ Multiplica (int p[L][C], int m[L][C], int o[L][C]) //função que gera e apresen
     int q, r;   //controladores de o
     int t = 0, u = 0;   //controladores de p (matriz produto)
     int a;   //variável que armazena o produto
-    
+
     for (i = 0; i < L; i++, t++)
     {
         for (r = 0; r < C; u++, r++)
         {
             for (a = 0, j = 0, q = 0; j < C; j++, q++)
                 a = a + (m[i][j] * o[q][r]);
-            
+
             p[t][u] = a;
             a = 0;
             j = 0, q = 0;
@@ -85,7 +105,7 @@ Multiplica (int p[L][C], int m[L][C], int o[L][C]) //função que gera e apresen
         j = 0;
         u = 0;
     }
-    
+
     printf ("Matriz produto\n");
     for (t = 0; t < L; t++)
     {
